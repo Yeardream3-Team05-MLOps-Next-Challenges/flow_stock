bc2cb6fdac6423be70169949c6fdbf12c2a61373
sanghun9634 <sanghun9634@gmail.com>
2024-08-04 01:47:36 +0900

    feat: deploy 중 에러 확인 위한 코드 추가

--- deploy.py
+++ deploy.py
@@ -1,14 +1,14 @@
 import os
-import requests
-import json
-import asyncio
+import logging
 
 from prefect import flow
 from prefect.deployments import DeploymentImage
 from prefect.client.schemas.schedules import CronSchedule
+from prefect.exceptions import PrefectException
 
-# stock.py의 함수와 태스크를 가져옵니다.
 from flow import hun_fetch_and_send_stock_data
+
+logging.getLogger("prefect").setLevel(logging.DEBUG)
 
 # @flow(name="fetch_and_send_stock_data_flow", log_prints=True)
 # async def hun_stock_flow():
@@ -34,22 +34,27 @@ from flow import hun_fetch_and_send_stock_data
 #     print('응답 내용:', response.text)
 
 if __name__ == "__main__":
-    hun_fetch_and_send_stock_data.deploy(
-        name="hun_tick2min_deploy",
-        work_pool_name="docker-agent-pool",
-        work_queue_name="docker-agent",
-        image=DeploymentImage(
-            name="hun-stock",
-            tag="0.1.0",
-            dockerfile="Dockerfile",
-            platform="linux/arm64",
-            buildargs={
-                        "APP_KEY": os.getenv("APP_KEY"),
-                        "APP_SECRET": os.getenv("APP_SECRET"),
-                        "HTS_ID": os.getenv("HTS_ID"),
-                        "KAFKA_URL": os.getenv("KAFKA_URL"),
-                        },
-        ),
-        schedule=(CronSchedule(cron="0 8 * * *", timezone="Asia/Seoul")),
-        build=True,
-    )
+    try:
+        hun_fetch_and_send_stock_data.deploy(
+            name="hun_tick2min_deploy",
+            work_pool_name="docker-agent-pool",
+            work_queue_name="docker-agent",
+            image=DeploymentImage(
+                name="hun-stock",
+                tag="0.1.0",
+                dockerfile="Dockerfile",
+                platform="linux/arm64",
+                buildargs={
+                            "APP_KEY": os.getenv("APP_KEY"),
+                            "APP_SECRET": os.getenv("APP_SECRET"),
+                            "HTS_ID": os.getenv("HTS_ID"),
+                            "KAFKA_URL": os.getenv("KAFKA_URL"),
+                            },
+            ),
+            schedule=(CronSchedule(cron="0 8 * * *", timezone="Asia/Seoul")),
+            build=True,
+        )
+    except PrefectException as e:
+        print(f"Prefect error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
